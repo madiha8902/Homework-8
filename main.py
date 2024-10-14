839f60b0ff16101463831e4deb14dd2997fdb429
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-10-14 19:35:25 +1100

    Homework 8 turtle

---
+++ main.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.Screen().bgcolor("white")
+
+s=turtle.Screen()
+s.setup(500,500)
+
+turtle.title("welcome to turtle")
+
+board=turtle.Turtle()
+
+board.fillcolor("black")
+board.begin_fill()
+board.forward(100)
+board.left(60)
+board.forward(100)
+board.left(60)
+board.forward(100)
+board.left(60)
+board.forward(100)
+board.left(60)
+board.forward(100)
+board.left(60)
+board.forward(100)
+board.left(60)
+board.end_fill()
+turtle.done()
